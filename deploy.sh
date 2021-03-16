b54e174db0d5ca9698807c4d731a5805df8ef042
Niall Tiernan <nialltiernan93@gmail.com>
2021-03-16 14:46:13 +0100

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo '#### Moving to project directory ####'
+cd /home/niall/Projects/free-to-lame
+
+echo '#### Pulling code from git ####'
+git pull
+
+echo '#### Installing composer packages ####'
+composer install --optimize-autoloader --no-dev
+
+echo '#### Removing config cache ####'
+rm data/cache/*.php
+
+echo '#### Deployment successful ####'
