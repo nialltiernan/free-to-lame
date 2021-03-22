ed1149e27ade34674aae0d94a2117acba3704267
Niall Tiernan <nialltiernan93@gmail.com>
2021-03-22 14:33:33 +0100

    Update deploy script and gitignore

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 echo '#### Moving to project directory ####'
-cd /home/niall/Projects/free-to-lame
+cd /home/niall/Projects/free-to-lame || return
 
 echo '#### Pulling code from git ####'
 git pull
 
 echo '#### Installing composer packages ####'
-composer install --optimize-autoloader --no-dev
+/usr/bin/php7.4 /usr/local/bin/composer install --optimize-autoloader --no-dev
 
 echo '#### Removing config cache ####'
 rm -f data/cache/*.php
