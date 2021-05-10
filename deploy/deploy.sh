8168748c0cb29cb2ff9254a5d0dfa2d1667ee2e5
Niall Tiernan <nialltiernan93@gmail.com>
2021-05-10 11:49:08 +0200

    Update deployment script for Vue

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -9,6 +9,15 @@ git pull
 echo '#### Installing composer packages ####'
 /usr/bin/php7.4 /usr/local/bin/composer install --optimize-autoloader --no-dev
 
+echo '#### Running migrations ####'
+/usr/bin/php7.4 vendor/bin/phinx migrate -e production -c config/phinx.php
+
+echo '#### Installing NPM packages ####'
+npm install
+
+echo '#### Running build script ####'
+npm run-script build
+
 echo '#### Removing config cache ####'
 rm -f data/cache/*.php
 
