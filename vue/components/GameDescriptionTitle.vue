2f06d93298cb9ff5f84879e1c0c3c23188c16106
Niall Tiernan <nialltiernan93@gmail.com>
2022-05-02 21:58:41 +0200

    Add platform logos

--- vue/components/GameDescriptionTitle.vue
+++ vue/components/GameDescriptionTitle.vue
@@ -1,15 +1,16 @@
 <template>
-  <div class="d-flex align-items-end">
+  <div class="d-flex justify-content-between align-items-center">
     <h1>{{ title }}</h1>
+    <img v-if="platformLogo" style="height: 2.5rem" :src="platformLogo" :alt="platform" />
+    <span v-else class="badge badge-secondary mx-1 mt-1">{{ platform }}</span>
   </div>
 
-  <p class="small text-muted">
-    {{ genre }}
-    {{ platform }}
-    {{ publisher }}
-    {{ developer }}
-    {{ releaseDate }}
-  </p>
+  <div class="d-flex flex-wrap mb-2">
+    <span class="badge badge-secondary mr-1 mt-1">{{ genre }}</span>
+    <span class="badge badge-secondary mx-1 mt-1">{{ publisher }}</span>
+    <span class="badge badge-secondary mx-1 mt-1">{{ developer }}</span>
+    <span class="badge badge-secondary ml-1 mt-1">{{ releaseDate }}</span>
+  </div>
 </template>
 
 <script>
@@ -40,6 +41,36 @@ export default {
       type: String,
       required: true
     },
+  },
+  data() {
+    return {
+      platformLogo: null,
+    }
+  },
+  computed: {
+    isPlatformWindows() {
+      return this.platform === 'Windows';
+    },
+    isPlatformBrowser() {
+      return this.platform === 'Web Browser';
+    },
+  },
+  methods: {
+    getWindowsLogo() {
+      return BASE_URL + '/img/windows-logo.svg';
+    },
+    getRandomBrowserLogo() {
+      const browsers = ['chrome', 'edge', 'firefox', 'safari'];
+      const browser = browsers[Math.floor(Math.random()*browsers.length)];
+      return BASE_URL + '/img/' + browser + '-logo.svg';
+    },
+  },
+  created() {
+    if (this.isPlatformWindows) {
+      this.platformLogo = this.getWindowsLogo();
+    } else if (this.isPlatformBrowser) {
+      this.platformLogo = this.getRandomBrowserLogo();
+    }
   }
 }
 </script>
