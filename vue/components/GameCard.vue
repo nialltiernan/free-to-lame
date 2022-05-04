2b7b79b9245fb91b271407bde9937898b59775ac
Niall Tiernan <ntiernan@nbeyond.nl>
2022-05-04 22:02:28 +0200

    Add platform link to GameCard

--- vue/components/GameCard.vue
+++ vue/components/GameCard.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="col">
     <div class="card shadow-sm">
-      <router-link :to="{ name: 'Game', params: { gameId:game.id} }"
-                   class="btn btn-sm btn-outline-secondary">
+      <router-link :to="{ name: 'Game', params: { gameId:game.id} }" class="btn btn-sm btn-outline-secondary">
         <img :src="game.thumbnail" class="img-fluid rounded" :alt="game.title">
       </router-link>
       <div class="card-body">
         <div class="d-flex justify-content-between align-items-center">
           <span class="badge badge-secondary mr-2 p-1">{{ game.genre }}</span>
-          <img v-if="platformLogo" style="height: 1.5rem" :src="platformLogo" :alt="game.platform" />
+          <router-link v-if="platformLogo" :to="{ name: 'Platform', params: {platform: routerPlatform} }">
+            <img style="height: 1.5rem" :src="platformLogo" :alt="game.platform" />
+          </router-link>
           <span v-else class="badge badge-secondary p-1">{{ game.platform }}</span>
         </div>
         <p class="card-text mt-1">{{ shortDescription }}</p>
@@ -41,6 +42,9 @@ export default {
     isPlatformBrowser() {
       return this.game.platform === 'Web Browser';
     },
+    routerPlatform() {
+      return this.isPlatformWindows ? 'pc' : 'browser';
+    }
   },
   methods: {
     getWindowsLogo() {
