57e02dab695916a3ab4d0b4dd4cc21ab82406915
Niall Tiernan <ntiernan@nbeyond.nl>
2022-05-05 08:50:12 +0200

    Add platform logos

--- vue/views/Platforms.vue
+++ vue/views/Platforms.vue
@@ -3,11 +3,13 @@
     <h1 class="mb-4">Platforms</h1>
 
     <div class="row">
-      <h2 class="col shadow-sm pb-2">
+      <h2 class="col shadow-sm pb-2 d-flex justify-content-between align-items-end">
         <router-link :to="{ name: 'Platform', params: {platform: 'browser'}}">Browser</router-link>
+        <img :src="browserImage" alt="browser" style="height: 2rem" class="ml-3" />
       </h2>
-      <h2 class="col shadow-sm pb-2">
+      <h2 class="col shadow-sm pb-2 d-flex justify-content-between align-items-end">
         <router-link :to="{ name: 'Platform', params: {platform: 'pc'}}">PC</router-link>
+        <img :src="windowsImage" alt="pc" style="height: 2rem" class="ml-3" />
       </h2>
     </div>
   </div>
@@ -16,12 +18,12 @@
 <script>
 export default {
   computed: {
-    pc() {
-      return BASE_URL + '/platform/pc'
+    windowsImage() {
+      return BASE_URL + '/img/windows-logo.svg';
     },
-    browser() {
-      return BASE_URL + '/platform/browser'
-    }
+    browserImage() {
+      return BASE_URL + '/img/internet-monitor.svg';
+    },
   },
 }
 </script>
