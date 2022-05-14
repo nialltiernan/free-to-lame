c8f6e9aad4c07c9069312dacd1b692840cd9b6b3
Niall Tiernan <ntiernan@nbeyond.nl>
2022-05-14 13:28:58 +0200

    Modal XL

--- vue/components/GameScreenshots.vue
+++ vue/components/GameScreenshots.vue
@@ -3,7 +3,7 @@
     <ImageCarousel carousel-id="screenshotCarousel" :images="screenshots"/>
 
     <div class="modal fade" id="screenshotModal" tabindex="-1" aria-labelledby="screenshotModalLabel" aria-hidden="true">
-      <div class="modal-dialog">
+      <div class="modal-dialog modal-xl">
         <div class="modal-content">
           <div class="modal-body">
             <ImageCarousel carousel-id="modalCarousel" :images="screenshots"/>
