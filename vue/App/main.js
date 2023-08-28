47cc9f0829c994d32284345a0f65482baecbd2ca
Niall Tiernan <ntiernan@nbeyond.nl>
2023-08-28 11:04:49 +0200

    Cleanup: create Store subfolder

--- vue/App/main.js
+++ vue/App/main.js
@@ -6,7 +6,7 @@ import '../../node_modules/equal-vue/dist/style.css'
 import App from './App.vue'
 
 import router from '../Router/router.js';
-import store from '../store.js'
+import store from '../Store/store.js'
 
 createApp(App)
     .use(router)
