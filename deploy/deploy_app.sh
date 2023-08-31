953ab1fcd72c1a6feceb8f62371679ebd1def76c
Niall Tiernan <ntiernan@nbeyond.nl>
2023-08-31 21:01:49 +0200

    [Android]: Update deployment script for app to copy to Cordova folder

--- deploy/deploy_app.sh
+++ deploy/deploy_app.sh
@@ -2,14 +2,26 @@
 
 echo '#### Deploying app ####'
 
-echo '## Removing previous assets'
+echo '## Local web app'
+echo '# Removing previous assets'
 rm -r ~/Projects/free-to-lame-app/public/css
 rm -r ~/Projects/free-to-lame-app/public/js
 rm -r ~/Projects/free-to-lame-app/public/dist
 
-echo '## Copying new assets'
+echo '# Copying new assets'
 cp -r ~/Projects/free-to-lame/public/css ~/Projects/free-to-lame-app/public
 cp -r ~/Projects/free-to-lame/public/js ~/Projects/free-to-lame-app/public
 cp -r ~/Projects/free-to-lame/public/dist ~/Projects/free-to-lame-app/public
+
+echo '## Cordova app'
+echo '# Removing previous assets'
+rm -r ~/Projects/free-to-cordova/www/css
+rm -r ~/Projects/free-to-cordova/www/js
+rm -r ~/Projects/free-to-cordova/www/dist
+
+echo '# Copying new assets'
+cp -r ~/Projects/free-to-lame/public/css ~/Projects/free-to-cordova/www
+cp -r ~/Projects/free-to-lame/public/js ~/Projects/free-to-cordova/www
+cp -r ~/Projects/free-to-lame/public/dist ~/Projects/free-to-cordova/www
 
 echo '#### Deployment successful ####'
